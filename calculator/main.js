21034762bbbd1f3402d8fb289d92e960af69091b
Prince34345 <priyanshudhawan55@gmail.com>
2025-10-06 14:47:58 +0530

    completed issue fixes

--- calculator/main.js
+++ calculator/main.js
@@ -5,13 +5,15 @@ const clear = document.getElementById("clear");
 const buttons = document.querySelectorAll(".number, .operator");
 let currentVal;
 const back = document.getElementById('back');
-
+const specialOperator = document.querySelectorAll('.specialOperator')
 
 input.addEventListener("input", (e) => {
   currentVal = e.target.value;
+  console.log(currentVal);
 });
 
 let result = "";
+let memory = 0
 
 answer.addEventListener("click", () => {
   input.value = answer.innerText;
@@ -24,12 +26,22 @@ clear.addEventListener("click", () => {
   currentVal = 0;
 });
 
+
+
 Equal.addEventListener("click", (e) => {
-  try {
+  
+  try {    
+    if (memory > 0) {
+       return;
+    }
     result = eval(currentVal);
+
     answer.innerHTML = result;
+    return;
   } catch (err) {
     input.value = "Error";
+    alert("Invalid Expression")
+    return;
   }
 });
 
@@ -46,6 +58,55 @@ buttons.forEach((btn) => {
     }
   });
 });
+
+
+
+let specialOps = {
+   "%": () => {
+          input.value = currentVal + "%";
+          let res = currentVal / 100;
+          currentVal = res;
+    },
+   "M+": () => {
+          let num = eval(currentVal);
+          memory += num;
+          currentVal = "";
+          input.value = "";    
+   },
+   'M-': () => {
+          let num = eval(currentVal);
+          memory -= num;
+          console.log(num, memory, currentVal)          
+          currentVal = "";
+          input.value = ""; 
+   },
+   "√": () => {
+          input.value = "√" + currentVal;
+          let res = Math.sqrt(currentVal);
+          currentVal = res;
+   },
+   "MR": () => {
+          currentVal = memory;
+          input.value = "";
+          answer.innerHTML = "=" + currentVal;
+   },
+   "MC": () => {
+          memory = 0;
+          answer.innerText = "=0"
+          currentVal = "0";
+   }
+}
+
+
+
+specialOperator.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const operator = btn.textContent.trim();
+      if (specialOps[operator]) {
+          specialOps[operator]();
+      }
+    })
+})
 
 
 back.addEventListener('click', () => {
